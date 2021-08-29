033271009c52e9fc744505c5eb7a3aadfbf848c3
Mathieu Longe <mathieu.longe@orange.fr>
2021-08-29 14:46:29 +1000

    students lists using portal, fragment and refs

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+// import Wrapper from './components/Helpers/Wrapper';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    // <Wrapper>
+    <React.Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </React.Fragment>
+    // </Wrapper>
+  );
+}
+
+export default App;
